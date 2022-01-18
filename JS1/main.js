ca5dacafe4fb6a279a19aabb9130868d89793428
Boryana Rakova <bubeto1q1@gmail.com>
2022-01-18 20:41:55 +0000

    Add java projiects 1-7

---
+++ JS1/main.js
@@ -0,0 +1,27 @@
+
+ //var Family = "Krumovi" ;
+ //var Family = Family.fontcolor("blue");
+  //Mom ="Boryana" ;
+  //Mom= Mom.fontcolor("red");
+  //Son ="Krum" ;
+  //Son = Son.fontcolor('green');
+  //Father="Krasi";
+ //Father=Father.fontcolor("yellow");
+ 
+ //document.write (Son);
+
+//window.alert(3+3);
+
+//var letter = "word ";  //variable one
+//var word = "letter";           //variable two 
+//letter="letter".fontcolor("red"); // I gave color to the string of "letter" variable
+//document.write(letter + "word");   // Then i concatenate variable and string in order to appear in the browser as concatenated  - expression.      
+//window.alert("word" +"letter");   //This is a statement                    
+
+
+
+  function nameFunc(){
+      document.getElementById("time").innerHTML = Date();
+
+
+  }
