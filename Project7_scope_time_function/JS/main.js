8ec3152cb77aa858b50cb02d2db7338b3cda29e0
Boryana Rakova <bubeto1q1@gmail.com>
2022-01-19 11:49:29 +0000

    project 7

--- Project7_scope_time_function/JS/main.js
+++ Project7_scope_time_function/JS/main.js
@@ -1,11 +1,106 @@
-//assignment #40
+//assignment 
+//--------------//
 
-function my_function(){
-    if (new Date().getHours() < 12) {
-        document.getElementById("try").innerHTML = "It is morning!";
-    }
-    if (new Date().getHours() >= 12) {
-        document.getElementById("try").innerHTML = "It is not morning!";
-    }
-}
 
+//function my_function(){
+  //  if (new Date().getHours() < 12) {
+     //   document.getElementById("try").innerHTML = "It is morning!";
+   // }
+    //if (new Date().getHours() >= 12) {
+    //    document.getElementById("try").innerHTML = "It is not morning!";
+    //}
+//}
+
+//assignment 
+//------------//
+
+
+
+//function Bubble_gum(){
+   // Money = document.getElementById("Money").value ;
+    //if (Money >= 5) {
+      //  Buy = "You have enough money to pop bubbles";
+    //}
+    //else {
+     //  Buy = "Not popping bubbles !!!";
+    //}
+    //document.getElementById("For").innerHTML = Buy;
+//}
+
+
+//Assignment
+//---------------//
+
+
+//function Time_function(){
+    //var Time = new Date().getHours();
+    //var Reply;
+    //if (Time < 12 == Time > 0){
+       // Reply = "It's morning time! ";
+    //}
+    //else if (Time > 12 == 18){
+       // Reply = "It's afternoon!";
+    //}
+    //else{
+       // Reply = "It's evening time!";
+    //}
+    //document.getElementById("Time_of_day").innerHTML = Reply;
+//}
+
+
+//Assignment Submission # 7
+
+//var A = 15;
+//function my_function(){
+   // document.write(10 + A + "<br>"); 
+//}                                      //----Local global and variable
+//function my_function2(){
+  //  document.write(A +20);
+//}
+//my_function();
+//my_function2();
+
+//-------------------------------------------------------//
+
+
+//function my_Function(){
+     //first = document.getElementById("first").value ;
+    //if (first <=10 ){
+       // second  = "It's working";                                        //function that includes an if statement
+        //document.getElementById("second").innerHTML = second ;
+    //}
+//}
+   
+
+//--------------------------------------------------------------//
+
+
+//function my_function(){
+    //var A = 15;
+    //document.write(10 + A + "<br>"); 
+//}                                      //----Intentionally write a function with an error in it and use the console.log() 
+//function my_function2(){
+   //document.write(A +20);
+//}
+//my_function();
+//my_function2();
+
+//--------------------------------------------------------------//
+
+
+
+//function MY_Time() {
+    //var Time = new Date().getHours();
+    //var Reply ;
+    //if (Time < 12 == Time > 0){
+       // Reply = "It is morning time !!!";
+   // }
+    //else if (Time > 12 == Time < 18 ){
+        //Reply = "It is afternoon!"                  //Time Function
+
+   // }
+    //else {
+    //    Reply = "It is evening!!!"
+    //}
+    //document.getElementById("Time_of_day").innerHTML = Reply;
+//}
