9b26dac2305546fe907818f028e8f67bd3a0c06c
Boryana Rakova <bubeto1q1@gmail.com>
2022-01-20 13:03:06 +0000

    num8

---
+++ Project8_string_methods/JS/main.js
@@ -0,0 +1,110 @@
+
+//Assignment # 44
+
+//var one = "This ";
+//var two = "is a ";
+//var three = "sentence !";
+//var all = one.concat(two ,three);
+//document.getElementById("Concat").innerHTML= all;
+
+//----------------------------------------------------//
+
+
+//Assignment # 45
+
+//function my_function(){
+  //  var one = "Here is a short sentence !";
+   // var result = one.slice(10,15);                                   //Slice
+    //document.getElementById("slice").innerHTML = result;
+//}
+
+//------------------------------------------------------//
+
+
+//Chalange 10
+
+//function My_function(){
+  //var result = document.getElementById("Up").innerHTML ;
+  //document.getElementById("Up").innerHTML = result.toUpperCase();        //UpperCase
+
+//}
+
+ //var text = "This is a simple text";
+ //var place = text.search("simple");
+ //document.getElementById("Search").innerHTML= place;        //Search() Method
+
+//-----------------------------------------------------//
+
+
+//Assignment # 46
+
+//function StringM(){
+    //var A = 123;                                          
+    //var B = A.toString();
+    //document.getElementById("String").innerHTML= B ;
+//}
+
+//This code is not working .I have tried many times ,but it just shows me numbers ,not string ,no quotes . 
+//i dont know where is my mystake.
+
+
+//-----------------------------------------------------//
+
+//Assignment # 47 
+
+//function My_Function(){
+    //var one = 12344.234567890;
+   // document.getElementById("Precis").innerHTML= one.toPrecision(5);             //toPrecision
+//}
+
+
+//---------------------------------------------------------//
+
+//Challenge # 11
+
+
+//var A = 12345;
+//var B = A.toFixed(1);
+//document.getElementById("Fix").innerHTML= B;    
+
+               //toFixed
+
+
+               //var A = "This is a text ."
+               //var B = A.valueOf();
+               //document.getElementById("ValueOf").innerHTML= B;
+             
+                 //valueOf() 
+
+ 
+//-------------------------------------------------------------//
+
+
+//Assignment Submission # 8
+
+
+//CONCAT()
+
+//var A = "This";
+//var B = " is another";
+//var C = " string !";
+//var result = A.concat(B,C);
+//document.getElementById("Concat").innerHTML= result;
+
+ //SLICE()
+
+// var A = "This is a poor text which have to be sliced .";
+ //var B = A.slice(5,15);                                     
+ //document.getElementById("Slice").innerHTML = B;
+
+ //TO STRING() - Still can't get that ... It just shows me the numbers...
+
+ //var A = 1234;
+ //var B = A.toString();
+ //document.getElementById("ToString").innerHTML = B;
+
+    //TO PRECISION()
+
+   // var A = 12345.123400998;
+    //var B = A.toPrecision(11);
+    //document.getElementById("Precision").innerHTML= B;
